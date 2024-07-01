a2301d31fc23409c386faec2b4a0e6eb34343a64
MAHESH KALDHONE <101930531+maheshkaldhone@users.noreply.github.com>
2024-07-01 16:31:50 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,9 +6,9 @@ cd /app/FE
 # initial startup by running react-script "start", name process "marketing"
 # --watch watches and restarts if files change
 
-PM2_HOME='/root/.pm2' pm2 delete FE
-PM2_HOME='/root/.pm2' pm2 start npm --name "FE" -- start
+PM2_HOME='/root/.nvm/versions/node/v18.20.3/bin/pm2' pm2 delete FE
+PM2_HOME='/root/.nvm/versions/node/v18.20.3/bin/pm2' pm2 start /root/.nvm/versions/node/v18.20.3/bin/npm --name "FE" -- start
 
 # freeze process list for automatic respawn
-PM2_HOME='/root/.pm2' pm2 save
+PM2_HOME='/root/.nvm/versions/node/v18.20.3/bin/pm2' pm2 save
 
