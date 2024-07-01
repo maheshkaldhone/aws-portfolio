b4772cb9af5c00ab2ddcf829177c5950d88e1387
MAHESH KALDHONE <101930531+maheshkaldhone@users.noreply.github.com>
2024-07-01 16:06:46 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /app/aws-portfolio/build
+cd /app/FE
 
 # initial startup by running react-script "start", name process "marketing"
 # --watch watches and restarts if files change
 
-PM2_HOME='/root/.pm2' pm2 delete portfolio
-PM2_HOME='/root/.pm2' pm2 start npm --name "portfolio" -- start
+PM2_HOME='/root/.pm2' pm2 delete FE
+PM2_HOME='/root/.pm2' pm2 start npm --name "FE" -- start
 
 # freeze process list for automatic respawn
 PM2_HOME='/root/.pm2' pm2 save
