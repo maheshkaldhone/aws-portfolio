9ef75b1cc0ff0d731e09b9e8a6aad33351c72aad
MAHESH KALDHONE <101930531+maheshkaldhone@users.noreply.github.com>
2024-07-01 16:06:00 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export FOLDER=/app/aws-portfolio
+export FOLDER=/app/FE
 
-if PM2_HOME='/root/.pm2' pm2 list | grep -q "portfolio"; then
-  # Delete the PM2 process for Bleezur-frontend
+if PM2_HOME='/root/.pm2' pm2 list | grep -q "FE"; then
+  # Delete the PM2 process for FE
   PM2_HOME='/root/.pm2' pm2 delete portfolio
   PM2_HOME='/root/.pm2' pm2 save
-  echo "Process 'portfolio' deleted from pm2."
+  echo "Process 'FE' deleted from pm2."
 else
-  echo "Process 'portfolio' not found in pm2."
+  echo "Process 'FE' not found in pm2."
 fi
 
 if [ -d $FOLDER ]; then
