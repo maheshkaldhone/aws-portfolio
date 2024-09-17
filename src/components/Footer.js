53386fb166c32ac14dbebeb4f8f8f539db3a19c4
Mahesh <maheshkaldhone1920@gmail.com>
2024-09-17 13:57:09 +0530

    removed unused variables 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 function Footer() {
-  let date = new Date();
   return (
     <Container fluid className="footer">
       {/* <Row>
